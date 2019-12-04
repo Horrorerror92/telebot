31e5bb07f8da2a5525e0fc99c9135ff55750736b
Sergey Zhitkevich <horrorerror92@gmail.com>
2019-12-04 21:46:59 +0300

    fix 3

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const TelegramBot = require('node-telegram-bot-api')
 
-//const TOKEN = process.env.TOKEN;
 const TOKEN = "997025459:AAEjEzITgsSEwZP6wr8k-6fymLVWY4LVDi8"
 
 const bot = new TelegramBot(TOKEN, { polling: true })
