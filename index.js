cf3a0c7282b60df9a80fb5b5e7116166aa6e2025
Sergey Zh <horrorerror92@gmail.com>
2019-12-05 16:28:38 +0300

    first setting webhook

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
+
+
+const TOKEN = process.env.TOKEN || '997025459:AAEjEzITgsSEwZP6wr8k-6fymLVWY4LVDi8';
 const TelegramBot = require('node-telegram-bot-api');
+const options = {
+  webHook: {
+    port: process.env.PORT || 5000
+  }
+};
 
-const TOKEN = "997025459:AAEjEzITgsSEwZP6wr8k-6fymLVWY4LVDi8";
+const url = process.env.APP_URL || 'https://expenses-telebot.herokuapp.com:443';
+const bot = new TelegramBot(TOKEN, options);
 
-const bot = new TelegramBot(TOKEN, { polling: true });
+bot.setWebHook(`${url}/bot${TOKEN}`);
 
 
+// Just to ping!
 bot.onText(/\/start/, function (msg, match) {
   var fromId = msg.from.id;
   bot.sendMessage(fromId, " Введите логин:");
