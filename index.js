21b7ff91b9610ca224b23b5089f0464aa070da4e
Sergey Zhitkevich <horrorerror92@gmail.com>
2019-12-05 21:44:20 +0300

    try to select data from database

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-
 const TOKEN = process.env.TOKEN || '997025459:AAEjEzITgsSEwZP6wr8k-6fymLVWY4LVDi8';
 const TelegramBot = require('node-telegram-bot-api');
 const options = {
@@ -13,10 +11,31 @@ const bot = new TelegramBot(TOKEN, options);
 
 bot.setWebHook(`${url}/bot${TOKEN}`);
 
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+
+client.connect();
+
+client.query('SELECT sfid,email,password__c,office__c FROM salesforce.contact;', (err, res) => {
+  if (err) throw err;
+  var count = 0;
+  for (let row of res.rows) {
+
+    console.log(JSON.stringify(row));
+  }
+
+  client.end();
+});
+
 
 // Just to ping!
 bot.onText(/\/start/, function (msg, match) {
   var fromId = msg.from.id;
-  bot.sendMessage(fromId, " Введите логин:");
+  bot.sendMessage(fromId, " Введите логин!:");
 
 });
