d991dbf24ed67c0d10d1fc0c1a47ecdbf3be48f0
Sergey Zhitkevich <horrorerror92@gmail.com>
2019-12-07 21:59:11 +0300

    try login and pass

--- index.js
+++ index.js
@@ -1,106 +1,123 @@
-const TOKEN = process.env.TOKEN;
+const config = require('config');
+const { Client } = require('pg');
+
+const token = config.get('token');
+const portFromConfig = config.get('port');
+const appUrl = config.get('appurl');
+const database = config.get('database');
+
+const TOKEN = process.env.TOKEN || token;
 const TelegramBot = require('node-telegram-bot-api');
 const options = {
   webHook: {
-    port: process.env.PORT
+    port: process.env.PORT || portFromConfig
   }
 };
 
-const url = process.env.APP_URL;
+const client = new Client({
+  connectionString: process.env.DATABASE_URL || database,
+  ssl: true
+});
+
+const url = process.env.APP_URL || appUrl;
 const bot = new TelegramBot(TOKEN, options);
+//const bot = new TelegramBot(TOKEN, { polling: true });
 
 bot.setWebHook(`${url}/bot${TOKEN}`);
-
-const { Client } = require('pg');
-
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true,
-});
-
-
-client.connect();
-
-client.query('SELECT sfid,email,password__c,office__c FROM salesforce.contact;', (err, res) => {
-  if (err) throw err;
-  var count = 0;
-  for (let row of res.rows) {
-
-    console.log(JSON.stringify(row));
-  }
-
-  client.end();
-});
-
 
 let passData = [];
 let success = undefined;
 
+var userState = new Object();
+userState.currentStep = 0;
 
 
 bot.onText(/\/start/, function (msg, match) {
+  if (userState.currentStep == 0) {
+    console.log("/start before" + userState.currentStep);
+    userState.currentStep = 1;
+    var fromId = msg.from.id;
+    initStart(fromId);
+  } else {
 
-  var fromId = msg.from.id;
-  initStart(fromId);
+    console.log(userState.currentStep);
+    var fromId = msg.from.id;
 
+  }
 
 });
 
 function initStart(fromId) {
 
-  bot.sendMessage(fromId, " Введите логин:");
+  if (userState.currentStep == 1) {
+
+    bot.sendMessage(fromId, "Введите логин:");
+    console.log(" before login" + userState.currentStep);
+    userState.currentStep = 2;
+  }
 
   bot.on('message', (msg) => {
+    console.log(" before before" + userState.currentStep);
+    if (userState.currentStep == 2) {
 
-    if (msg.text != '/start') {
+      console.log(" before pass" + userState.currentStep);
 
       if (msg.text && passData.length < 2) {
 
         if (passData.length < 1) {
           bot.sendMessage(fromId, " Введите пароль:");
+          userState.currentStep == 3;
+          console.log(userState.currentStep);
         }
 
         passData.push(msg.text);
       }
-      if (passData.length == 2) {
+      if (passData.length == 2 && userState.currentStep == 3) {
 
-        //server logic
-        success = true;
-      }
-      if (success == true) {
-        bot.sendMessage(fromId, " Авторизация прошла успешно!",
-          {
-            reply_markup: {
-              inline_keyboard: [
-                [
-                  {
-                    text: "Текущий баланс",
-                    callback_data: 'currentBalance'
-                  },
-                  {
-                    text: "Создать карточку",
-                    callback_data: 'createCard'
-                  }
-                ]
-              ]
-            }
-          });
-      }
-      if (success == false) {
-        bot.sendMessage(fromId, " Неправильный логин и/или пароль!");
-      }
+        console.log(userState.currentStep);
 
+        let valueLogin = passData[0];
+        let valuePass = passData[1];
+
+        getRightData(valueLogin, valuePass, function (result) {
+
+          if (result.length != 0) {
+            bot.sendMessage(fromId, " Неправильный логин и/или пароль!");
+            userState.currentStep = 0;
+            console.log(userState.currentStep);
+
+          } else {
+
+            bot.sendMessage(fromId, " Авторизация прошла успешно!",
+              {
+                reply_markup: {
+                  inline_keyboard: [
+                    [
+                      {
+                        text: "Текущий баланс",
+                        callback_data: 'currentBalance'
+                      },
+                      {
+                        text: "Создать карточку",
+                        callback_data: 'createCard'
+                      }
+                    ]
+                  ]
+                }
+              });
+          }
+
+        });
+      }
     }
-
-
   });
 
 }
 
-
 bot.on('callback_query', query => {
 
   const id = query.message.chat.id;
+  console.log(query);
   console.log(query.data);
   if (query.data.toUpperCase() === 'CURRENTBALANCE') {
 
@@ -154,4 +171,37 @@ bot.on('callback_query', query => {
   }
 
 });
+
+
+function getRightData(valueLogin, valuePass, dataCallback) {
+
+  client.connect();
+
+  client.query(`SELECT sfid,email,password__c,office__c FROM salesforce.contact WHERE email = '${valueLogin}'
+  AND password__c = '${valuePass}';`, (err, res) => {
+
+    var tempArr = [];
+    if (res.rows.length == 0) {
+      return dataCallback(tempArr);
+    } else {
+
+      for (let [keys, values] of Object.entries(res.rows)) {
+
+        for (let [key, value] of Object.entries(values)) {
+          tempArr.push(value);
+
+        }
+      }
+      return dataCallback(tempArr);
+    }
+
+  });
+
+}
+
+
+
+
+
+
 
